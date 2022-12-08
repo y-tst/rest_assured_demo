4a2798d6c65590c69376337b96b004ed1989191f
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-12-08 23:30:41 +0200

    First test + additional check

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -1,10 +1,12 @@
 package api;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
@@ -21,6 +23,20 @@ public class ReqresTest {
                 .log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
 
-        int i = 0;
+        users.forEach(x -> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        // the same:
+        Assertions.assertTrue(users.stream().allMatch(x -> x.getAvatar().contains(x.getId().toString())));
+
+        Assertions.assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
+
+
+        // 2nd variant of checked below:
+        List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x -> x.getId().toString()).collect(Collectors.toList());
+
+        for (int i = 0; i < avatars.size(); i++) {
+            Assertions.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
+
     }
 }
